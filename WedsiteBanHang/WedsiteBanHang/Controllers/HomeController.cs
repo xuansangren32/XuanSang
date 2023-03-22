ed2ddc2d58f05d665f16da174ab86e7220239367
conkunhua <renlotlong@gmail.com>
2023-03-23 03:06:40 +0700

    first commit

--- WedsiteBanHang/WedsiteBanHang/Controllers/HomeController.cs
+++ WedsiteBanHang/WedsiteBanHang/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using WedsiteBanHang.Context;
@@ -20,7 +23,91 @@ namespace WedsiteBanHang.Controllers
             objHomeModel.ListProduct = objWedBanHangEntities.Products.ToList();
             return View(objHomeModel);
         }
+        [HttpGet]
+     
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = objWedBanHangEntities.Users.FirstOrDefault(s => s.Email==_user.Email);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    objWedBanHangEntities.Configuration.ValidateOnSaveEnabled = false;
+                    objWedBanHangEntities.Users.Add(_user);
+                    objWedBanHangEntities.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exitsts";
+                    return View();
+                }
+            }
+            return View("Index");
+        }
 
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+           
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fromData = Encoding.UTF8.GetBytes(str);
+                byte[] targetData = md5.ComputeHash(fromData);
+                string byte2String = null;
+
+                for (int i = 0; i < targetData.Length; i++)
+                {
+                    byte2String += targetData[i].ToString("x2");
+
+                }
+                return byte2String;
+            }
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(password);
+                var data = objWedBanHangEntities.Users.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["idUser"] = data.FirstOrDefault().Id;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
